fce8f5041c1f405589252b8a7eddd94a1faa4caa
Pavel Ilchev <pavel_ilchev@abv.bg>
2016-08-03 16:59:47 +0300

    account upgrade

--- Hotel.App/Controllers/AccountController.cs
+++ Hotel.App/Controllers/AccountController.cs
@@ -150,7 +150,7 @@
         {
             if (this.ModelState.IsValid)
             {
-                var user = new User { UserName = model.Email, Email = model.Email };
+                var user = new User { UserName = model.Username, Email = model.Email };
                 var result = await this.UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- Hotel.App/Models/ViewModels/AccountViewModels.cs
+++ Hotel.App/Models/ViewModels/AccountViewModels.cs
@@ -70,6 +70,11 @@
         public string Email { get; set; }
 
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 1)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
