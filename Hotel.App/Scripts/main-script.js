01413fe04856f0350151dd6adfa629d250c2b9c7
Pavel Ilchev <pavel_ilchev@abv.bg>
2016-08-08 16:56:55 +0300

    work on create review

--- Hotel.App/Scripts/main-script.js
+++ Hotel.App/Scripts/main-script.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function () {
 
+    // Home page slideshow settings
     $("#owl-demo").owlCarousel({
         autoPlay: 4000,
         navigation: false, // Show next and prev buttons
@@ -14,5 +15,27 @@
         // itemsTablet: false,
         // itemsMobile : false
 
+    });
+});
+
+
+// Create a review chose rating
+$(document).ready(function () {
+    $("li").hover(function () {
+        var myClass = $(this).attr("class");
+        if (myClass == "creare-review-1star") {
+            $(this).parent("ul").css("background", 'url("/Files/Images/raiting-1star.jpg")');
+        } else if (myClass == "creare-review-2star") {
+            $(this).parent("ul").css("background", 'url("/Files/Images/raiting-2star.jpg")');
+        } else if (myClass == "creare-review-3star") {
+            $(this).parent("ul").css("background", 'url("/Files/Images/raiting-3star.jpg")');
+        } else if (myClass == "creare-review-4star") {
+            $(this).parent("ul").css("background", 'url("/Files/Images/raiting-4star.jpg")');
+        } else if (myClass == "creare-review-5star") {
+            $(this).parent("ul").css("background", 'url("/Files/Images/stars_sprite_icon.png") no-repeat 0 0');
+        }  
+        
+        var raiting = $(this).attr("value");
+        document.getElementById("setRating").setAttribute("value", raiting);
     });
 });
