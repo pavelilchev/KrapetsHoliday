bdeb3ae16e6577a25849f5a66dda24d71fe0dffb
Pavel Ilchev <pavel_ilchev@abv.bg>
2016-08-12 20:07:40 +0300

    string extension phone beauty

---
+++ Hotel.App/Utils/Extensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Hotel.App.Utils
+{
+    public static class Extensions
+    {
+        public static string BeautifyPhoneNumber(this string str)
+        {
+            int position = str.Length - 3;
+            while (position > 3)
+            {
+                str = str.Insert(position, " ");
+                position -= 3;
+            }
+
+            return str;
+        }
+    }
+}
